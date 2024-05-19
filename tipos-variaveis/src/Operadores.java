b3425107f788304f75ec67354c9cdb77a1483b4c
fabliny-ab <fabliny.ab@gmail.com>
2024-05-18 21:38:16 -0300

    fazendo exercicio sistema smart Tv

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,23 @@
+public class Operadores {
+    
+    public static void main(String[] args) {
+
+        // String concatenacao = "?";
+
+        // concatenacao = 1 + 1 + "1" + 1;
+
+        // System.out.println(concatenacao);
+
+        // String nomeCompleto = "LINGUAGEM " + "JAVA";
+
+        // System.out.println (nomeCompleto);
+
+        int numero = 5;
+
+        numero = -numero;
+
+        numero =  -(numero);
+
+        System.out.println(numero);
+    }
+}
